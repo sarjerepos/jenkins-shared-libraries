86618f02e6c2edba87130341e9afd72a46c756cf
Sarjerao <115054855+sarjerepos@users.noreply.github.com>
2024-10-31 21:48:29 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,8 +2,8 @@ def call(String Project, String ImageTag, String dockerhubuser){
     withCredentials([usernamePassword('credentialsId':"docker",
                                 passwordVariable:"dockerHubPass",
                                 usernameVariable:"dockerHubUser")]){
-                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-                sh "docker image tag notes-app:latest ${env.dockerHubUser}/notes-app:latest"
-                sh "docker push ${env.dockerHubUser}/notes-app:latest"
+                sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+                sh "docker image tag ${Project}:${ImageTag} ${dockerHubUser}/${Project}:${ImageTag}"
+                sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
 				}  
 }
